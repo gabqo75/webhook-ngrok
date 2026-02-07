6cbb1571300ba22b0d19b2fd0d9bdba2d62e0451
Gabriel QUÉRO <gabriel.quero@efrei.net>
2026-02-07 20:05:19 +0100

    Update header text in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>React est déployé.</h1>
+      <h1>React est déployé. Modif 1</h1>
     </div>
   </React.StrictMode>
 )
